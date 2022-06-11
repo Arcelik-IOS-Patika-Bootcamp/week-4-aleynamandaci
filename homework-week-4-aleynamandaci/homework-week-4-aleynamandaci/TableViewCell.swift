82957fd0cf8ed9c7cc51b8a3ed66375a4419c589
Aleyna Mandacı <aleynamandaci@gmail.com>
2022-06-11 19:43:20 +0300

    added TableViewCell

---
+++ homework-week-4-aleynamandaci/homework-week-4-aleynamandaci/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  homework-week-4-aleynamandaci
+//
+//  Created by Aleyna Mandacı on 11.06.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
